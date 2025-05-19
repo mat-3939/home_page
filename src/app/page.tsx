eeda775a197f6b4400c71edd7b7774f213e924d1
mat-3939 <znuakierkto@ymail.ne.jp>
2025-05-20 08:56:17 +0900

    README変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,18 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 export default function Home() {
   return (
-    <>
+  <>
       <Navbar />
-      <header className={styles.top}>
-        <h1 className={styles.top_msg}>bluecode, Lifestyle developer.</h1>
-      </header>
-      <main>
+    <header className={styles.top}>
+      <h1 className={styles.top_msg}>bluecode, Lifestyle developer.</h1>
+    </header>
+    <main>
         <section className={styles.content}>
           <h2>websiteの最新情報</h2>
           <p>最新の情報をご案内します。</p>
         </section>
-      </main>
+    </main>
       <Footer />
-    </>
+  </>
   );
 }
